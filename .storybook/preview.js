c03c0e27026fe2bf1d23dc89f5fef74c1d059638
Vinícius Hoyer <contact@vhoyer.dev>
2022-07-13 16:47:10 -0300

    set index for docs panel on previewTabs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,4 +6,10 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  previewTabs: {
+    'storybook/docs/panel': {
+       // position "Docs" tab before "Canvas"
+      index: -1
+    },
+  },
 }
